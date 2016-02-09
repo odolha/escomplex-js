f005b58f959c21e802a53f97e0e114c86c3387e8
Ovidiu Dolha <ovidiu.dolha@isdc.eu>
2016-02-09 15:38:27 +0200

    added flexible ecmaOptions - to be passed from the consumer, e.g. version and specific options

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # escomplex-js
 
+**This is a fork of the original**
+
+It allows sending out ecmaOptions params:
+	
+	options.ecmaOptions= {
+		ecmaVersion: 6, 			// the version preset to use
+		sourceType: 'module', 		// module or script
+		tolerant: true,				// whether tolerant mode is enabled
+		ecmaFeatures: { ... }		// specific list of features to enable/disable
+	}
+	
+	analyse(..., options)
+
 [![Build status][ci-image]][ci-status] [![Dependency status][dep-image]][dep-status] [![devDependency status][devdep-image]][devdep-status]
 
 JavaScript wrapping library
--- src/index.js
+++ src/index.js
@@ -12,51 +12,50 @@ escomplex = require('escomplex');
 exports.analyse = analyse;
 
 function analyse (source, options) {
-    if (check.array(source)) {
-        return analyseSources(source, options);
-    }
+  if (check.array(source)) {
+    return analyseSources(source, options);
+  }
 
-    return analyseSource(source, options);
+  return analyseSource(source, options);
 }
 
 function analyseSources (sources, options) {
-    return performAnalysis(
-        sources.map(
-            mapSource.bind(null, options)
-        ).filter(filterSource),
-        options
-    );
+  return performAnalysis(
+    sources.map(
+      mapSource.bind(null, options)
+    ).filter(filterSource),
+    options
+  );
 }
 
 function mapSource (options, source) {
-    try {
-        return {
-            path: source.path,
-            ast: getSyntaxTree(source.code)
-        };
-    } catch (error) {
-        if (options.ignoreErrors) {
-            return null;
-        }
-
-        error.message = source.path + ': ' + error.message;
-        throw error;
+  try {
+    return {
+      path: source.path,
+      ast: getSyntaxTree(source.code, options.ecmaOptions)
+    };
+  } catch (error) {
+    if (options.ignoreErrors) {
+      return null;
     }
+
+    error.message = source.path + ': ' + error.message;
+    throw error;
+  }
 }
 
-function filterSource (source) {
-    return !!source;
+function filterSource(source) {
+  return !!source;
 }
 
-function getSyntaxTree (source) {
-    return esprima.parse(source, { loc: true });
+function getSyntaxTree(source, ecmaOptions) {
+  return esprima.parse(source, { loc: true, ecmaVersion: ecmaOptions.ecmaVersion, sourceType: ecmaOptions.sourceType, tolerant: ecmaOptions.tolerant, ecmaFeatures: ecmaOptions.ecmaFeatures });
 }
 
 function performAnalysis (ast, options) {
-    return escomplex.analyse(ast, walker, options);
+  return escomplex.analyse(ast, walker, options);
 }
 
 function analyseSource (source, options) {
-    return performAnalysis(getSyntaxTree(source), options);
+  return performAnalysis(getSyntaxTree(source, options.ecmaOptions), options);
 }
-
